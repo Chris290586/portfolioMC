b087f359121ac03ce25ac330a30b1410a1f63b02
Chris290586 <almeida.christiane@escola.pr.gov.br>
2025-11-19 10:10:19 -0300

    else

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let texto = document.querySelector("#texto");
-texto.textContent = "Christiane";
+
 
 let nome = prompt("Qual o seu nome");
 texto.textContent = nome;
+if(nome == null){
+    texto.textContent = "Seja bem-vindo(a)!";
+}
+else{
+    texto.textContent = nome;
+}
